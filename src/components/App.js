e4c7bcc1a2f9ede6475b3a178658ba533da3cc64
mimipeshy <perisndanu@gmail.com>
2021-04-06 21:45:45 +0300

    Implement App component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BooksForm from '../containers/BooksForm';
+import BookLists from '../containers/BookLists';
+
+function App() {
+  return (
+    <div>
+      <BookLists />
+      <BooksForm />
+    </div>
+  );
+}
+export default App;
