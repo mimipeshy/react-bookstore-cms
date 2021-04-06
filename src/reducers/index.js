e9cf26915c2b7d76b2d7f0e69e7d1e12c973b712
mimipeshy <perisndanu@gmail.com>
2021-04-06 21:50:51 +0300

    Implement reducers with create_book and remove_book actions

---
+++ src/reducers/index.js
@@ -0,0 +1,39 @@
+const initialBooks = [
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Divergent',
+    category: 'Action',
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Becoming',
+    category: 'Biography',
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Genghis Khan and the Making of the Modern World',
+    category: 'History',
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Dracula',
+    category: 'Horror',
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'The Tale of Peter Rabbit',
+    category: 'Kids',
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'Think and Grow Rich',
+    category: 'Learning',
+  },
+  {
+    id: Math.floor(Math.random() * 1000),
+    title: 'The Lord of The Rings',
+    category: 'Sci-Fi',
+  },
+];
+
+export default initialBooks;
